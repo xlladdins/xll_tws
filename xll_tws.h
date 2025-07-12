4e4318a8e3833cd80cf6befce72e6ffdb04640a2
Keith A. Lewis <kal@kalx.net>
2025-07-12 17:10:19 -0400

    libbid

--- xll_tws.h
+++ xll_tws.h
@@ -1,5 +1,6 @@
 // xll_tws.h - Trader Work Station API for Excel
 #pragma once
+#pragma warning(disable: 4267)
 #include "tws_api/EWrapper.h"
 #include "tws_api/EClientSocket.h"
 #include "xll24/include/xll.h"
